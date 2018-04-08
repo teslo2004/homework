2142cd09492a2f07c7dc3bc83da082b864b1ae80
giplash <anashkinviktar@gmail.com>
2018-04-08 22:04:56 +0300

    card game v1.0

--- lesson_8/2.js
+++ lesson_8/2.js
@@ -1,96 +1,97 @@
-let n = 30; // размер лабиринта
-let labirynth = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-  [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1],
-  [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-  [1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-  [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1],
-  [1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
-  [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
-  [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],
-  [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1],
-  [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
-  [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
-  [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
-  [1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-  [1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-  [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1],
-  [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1],
-  [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-  [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
-  [1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1],
-  [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
-  [1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-  [1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
-let tbody = document.querySelector("tbody");
-for (let i = 0; i < n; i++) {
-  const tr = document.createElement("tr");
-  tbody.appendChild(tr);
-  for (let j = 0; j < n; j++) {
-    const td = document.createElement("td");
-    tr.appendChild(td);
-    if (labirynth[i][j]) td.classList.add("block");
-    td.setAttribute("i", i);
-    td.setAttribute("j", j);
+(function() {
+  let n = 30; // размер лабиринта
+  let labirynth = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1],
+    [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ]
+  let tbody = document.querySelector("tbody");
+  for (let i = 0; i < n; i++) {
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    for (let j = 0; j < n; j++) {
+      const td = document.createElement("td");
+      tr.appendChild(td);
+      if (labirynth[i][j]) td.classList.add("block");
+      td.setAttribute("i", i);
+      td.setAttribute("j", j);
+    }
   }
-}
 
-const findNode = (i, j) => {
-  return document.querySelector(`[i='${i}'][j='${j}']`)
-}
-
-let target = findNode(1, 1);
-target.classList.add("target");
-let finish = findNode(n-2, n-1);
-finish.classList.add("finish");
-
-document.querySelector("body").addEventListener("keydown", function(e) {
-  let i = target.getAttribute("i");
-  let j = target.getAttribute("j");
-  let next;
-  switch (e.keyCode) {
-    case 39:
-      target.classList.remove("target");
-      if (j != n-1) j++;
-      next = findNode(i, j);
-      target = next.classList.contains("block") ? target : next;
-      target.classList.add("target");
-      e.preventDefault();
-      break;
-    case 37:
-      target.classList.remove("target");
-      if (j != 0) j--;
-      next = findNode(i, j);
-      target = next.classList.contains("block") ? target : next;
-      target.classList.add("target");
-      e.preventDefault();
-      break;
-    case 38:
-      target.classList.remove("target");
-      if (i != 0) i--;
-      next = findNode(i, j);
-      target = next.classList.contains("block") ? target : next;
-      target.classList.add("target");
-      e.preventDefault();
-      break;
-    case 40:
-      target.classList.remove("target");
-      if (i != n-1) i++;
-      next = findNode(i, j);
-      target = next.classList.contains("block") ? target : next;
-      target.classList.add("target");
-      e.preventDefault();
-      break;
+  const findNode = (i, j) => {
+    return document.querySelector(`[i='${i}'][j='${j}']`)
   }
-  if (target == finish) alert("win");
-});
 
+  let target = findNode(1, 1);
+  target.classList.add("target");
+  let finish = findNode(n-2, n-1);
+  finish.classList.add("finish");
+
+  document.querySelector("body").addEventListener("keydown", function(e) {
+    let i = target.getAttribute("i");
+    let j = target.getAttribute("j");
+    let next;
+    switch (e.keyCode) {
+      case 39:
+        target.classList.remove("target");
+        if (j != n-1) j++;
+        next = findNode(i, j);
+        target = next.classList.contains("block") ? target : next;
+        target.classList.add("target");
+        e.preventDefault();
+        break;
+      case 37:
+        target.classList.remove("target");
+        if (j != 0) j--;
+        next = findNode(i, j);
+        target = next.classList.contains("block") ? target : next;
+        target.classList.add("target");
+        e.preventDefault();
+        break;
+      case 38:
+        target.classList.remove("target");
+        if (i != 0) i--;
+        next = findNode(i, j);
+        target = next.classList.contains("block") ? target : next;
+        target.classList.add("target");
+        e.preventDefault();
+        break;
+      case 40:
+        target.classList.remove("target");
+        if (i != n-1) i++;
+        next = findNode(i, j);
+        target = next.classList.contains("block") ? target : next;
+        target.classList.add("target");
+        e.preventDefault();
+        break;
+    }
+    if (target == finish) alert("win");
+  });
+}());
--- lesson_8/3.js
+++ lesson_8/3.js
@@ -1,5 +1,10 @@
 const game = document.querySelector(".card-game");
-const cardAmount = 6;
+const cardAmount = 9;
+for (let i = 0; i < cardAmount * 2; i++) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  game.appendChild(card);
+}
 const cards = [];
 for (let i = 0, k = 1; i < cardAmount * 2; i += 2, k++) {
   cards[i] = "card" + (k);
@@ -16,32 +21,44 @@ const mix = arr => {
     replace(arr, i, random(0, arr.length-1));
   }
 }
+
 mix(cards);
 mix(cards);
 mix(cards);
-mix(cards);
+
 for (let i = 0; i < game.children.length; i++) {
   game.children[i].classList.add(cards[i]);
 }
 
 let first, second;
-/*
+let counter = 0;
+let cont = true; //continue
 game.addEventListener("click", function(e) {
-  e.target.classList.add("opened");
-  if (!second) {
-    first = e.target;
-    second = first;
-  }
-  else {
-    first = second;
-    second = e.target;
-    if (first.classList[1] == second.classList[1]) {
-      first.remove();
-      second.remove();
-    } /*else {
-      first.classList.remove("opened");
-      second.classList.remove("opened");
-    }*/
+  if (cont) {
+    e.target.classList.add("opened");
+    counter++;
+    if (!second) {
+      first = e.target;
+      second = first;
+    } else {
+      first = second;
+      second = e.target;
+      cont = false;
+      if (first.classList[1] == second.classList[1]) {
+        const timeout1 = window.setTimeout(function() {
+          first.remove();
+          second.remove();
+        }, 500);
+      }
+      if (counter == 2) {
+        const timeout1 = window.setTimeout(function() {
+          first.classList.remove("opened");
+          second.classList.remove("opened");
+          counter = 0;
+          second = null;
+          cont = true;
+        }, 500);
+      }
+    }
   }
 });
-*/
